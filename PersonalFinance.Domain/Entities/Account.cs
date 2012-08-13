f1dcd18cf722c1d64bce003d892e5da992d55d87
Carlos Forti <carlos.forti@foriteforti.com.br>
2012-08-13 13:54:29 -0300

    Account Class

---
+++ PersonalFinance.Domain/Entities/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalFinance.Domain.Entities
+{
+    [Table("Accounts")]
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="O campo é obrigatório")]
+        [StringLength(100, ErrorMessage="O campo deve ter no máximo 100 caracteres")]
+        [Display(Name="Nome")]
+        public string Name { get; set; }
+    }
+}
